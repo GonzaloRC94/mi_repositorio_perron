35406aca74358d2dbc5a91920c8850dab5a3ce27
GonzaloRC94 <124947546+gonzalorc94@users.noreply.github.com>
2023-02-09 20:21:14 -0600

    mi cambio para practica 03

---
+++ python_03_GRC.py
@@ -0,0 +1,10 @@
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+ciudad = input("¿En qué ciudad te ubicas? ")
+estado = input("¿Estás casado?")
+n1 = int(input("Ingresa un número del 1-100: "))
+n2 = int(input("Ingresa un número del 100-200: "))
+resultado = n1/n2
+
+
+print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {resultado}")
