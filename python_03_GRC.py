ff65b56fa3c6caee8b89c2aff78c970ddc2794b4
Practica_03 <gonzaloregalado.94@gmail.com>
2023-02-14 19:31:26 -0700

    mi cambio para practica_03

--- python_03_GRC.py
+++ python_03_GRC.py
@@ -6,5 +6,4 @@ n1 = int(input("Ingresa un número del 1-100: "))
 n2 = int(input("Ingresa un número del 100-200: "))
 resultado = n1/n2
 
-
 print(f"Mi nombre es {nombre}, mi edad es {edad}, soy de la ciudad de {ciudad} y mi resultado es {resultado}")
